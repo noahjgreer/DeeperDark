71a2097f15501503aea6890575d8a75dc9096a5a
Noah Greer <noahjgreer@gmail.com>
2026-01-16 18:39:31 -0600

    Revert "1.5.2-0b"
    This reverts commit a6b0cb378d5c7da77de70dc6c0ed3359b80ccc98.

--- src/main/java/net/noahsarch/deeperdark/event/BeaconHandler.java
+++ src/main/java/net/noahsarch/deeperdark/event/BeaconHandler.java
@@ -1,7 +1,6 @@
 package net.noahsarch.deeperdark.event;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.world.ServerWorld;
 import net.noahsarch.deeperdark.state.ActiveBeaconState;
 
@@ -13,13 +12,6 @@ public class BeaconHandler {
                  ActiveBeaconState.get(world).tick(world);
              }
          });
-
-         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-             // Check all worlds because beacons in any world could be tracking this player
-             for (ServerWorld world : server.getWorlds()) {
-                 ActiveBeaconState.get(world).onPlayerJoin(handler.player);
-             }
-             net.noahsarch.deeperdark.event.PlayerTickHandler.onPlayerJoin(handler.player);
-         });
     }
 }
+
