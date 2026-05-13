73adbf70a46ea299ea6b7f2a83bb93af9b26d01b
Noah Greer <noahjgreer@gmail.com>
2026-05-13 14:49:58 -0500

    bugfix

--- src/main/java/net/noahsarch/deeperdark/mixin/ServerPlayerHurtSoundMixin.java
+++ src/main/java/net/noahsarch/deeperdark/mixin/ServerPlayerHurtSoundMixin.java
@@ -1,6 +1,7 @@
 package net.noahsarch.deeperdark.mixin;
 
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.server.level.ServerPlayer;
 import net.noahsarch.deeperdark.sound.PlayerSoundManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,12 +9,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerPlayer.class)
+@Mixin(LivingEntity.class)
 public abstract class ServerPlayerHurtSoundMixin {
 
     @Inject(method = "playHurtSound", at = @At("HEAD"), cancellable = true)
     private void deeperdark$replaceHurtSound(DamageSource source, CallbackInfo ci) {
-        ServerPlayer self = (ServerPlayer) (Object) this;
+        if (!((Object) this instanceof ServerPlayer self)) return;
         if (PlayerSoundManager.playHurtSound(self)) {
             ci.cancel();
         }
