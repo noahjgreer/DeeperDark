082cd155992cef988b06b513c89743eb6de0bf31
Noah Greer <noahjgreer@gmail.com>
2026-05-24 13:00:10 -0500

    Worldborder now prevents external spawning

--- src/main/java/net/noahsarch/deeperdark/mixin/SpawnHelperMixin.java
+++ src/main/java/net/noahsarch/deeperdark/mixin/SpawnHelperMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.world.level.NaturalSpawner;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.noahsarch.deeperdark.event.WorldBorderHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,9 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class SpawnHelperMixin {
     @Inject(method = "isValidSpawnPostitionForType", at = @At("HEAD"), cancellable = true)
     private static void checkWorldBorder(ServerLevel world, MobCategory group, StructureManager structureAccessor, ChunkGenerator chunkGenerator, MobSpawnSettings.SpawnerData spawnEntry, BlockPos.MutableBlockPos pos, double squaredDistance, CallbackInfoReturnable<Boolean> cir) {
-        if (!WorldBorderHandler.isSafe(pos.getX(), pos.getZ())) {
+        if (!world.getWorldBorder().isWithinBounds(pos.getX(), pos.getZ())) {
             cir.setReturnValue(false);
         }
     }
 }
-
